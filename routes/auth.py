482f9c9be89dec1b26705032dab0c9f5976c7e08
Ganesh Macherla <myselfganesh65@gmail.com>
2026-06-26 13:14:20 +0530

    implement user registration with password hashing

--- routes/auth.py
+++ routes/auth.py
@@ -1,5 +1,10 @@
 from flask import Blueprint, request
 
+from models.user import User
+from extensions import db
+
+from werkzeug.security import generate_password_hash
+
 auth_bp = Blueprint("auth", __name__)
 
 
@@ -12,8 +17,36 @@ def register():
     email = data.get("email")
     password = data.get("password")
 
+    if not username or not email or not password:
+        return {
+            "error": "All fields are required"
+        }, 400
+
+    existing_email = User.query.filter_by(email=email).first()
+
+    if existing_email:
+        return {
+            "error": "Email already registered"
+        }, 409
+
+    existing_username = User.query.filter_by(username=username).first()
+
+    if existing_username:
+        return {
+            "error": "Username already taken"
+        }, 409
+
+    hashed_password = generate_password_hash(password)
+
+    new_user = User(
+        username=username,
+        email=email,
+        password_hash=hashed_password
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
     return {
-        "message": "Data received",
-        "username": username,
-        "email": email
-    }
+        "message": "User registered successfully"
+    }, 201
