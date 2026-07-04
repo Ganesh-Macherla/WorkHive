254bd9a983bfaa1ebeeda6f85710880ec8897326
Ganesh Macherla <myselfganesh65@gmail.com>
2026-07-04 20:23:33 +0530

    Display user hives on dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,7 +1,31 @@
 import { useNavigate } from "react-router-dom";
+import { useEffect, useState } from "react";
+import api from "../services/api";
 
 function Dashboard() {
   const navigate = useNavigate();
+
+  const [hives, setHives] = useState([]);
+
+  const fetchHives = async () => {
+    try {
+      const token = localStorage.getItem("token");
+
+      const response = await api.get("/hives", {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+
+      setHives(response.data);
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
+  useEffect(() => {
+    fetchHives();
+  }, []);
 
   return (
     <div>
@@ -26,6 +50,30 @@ function Dashboard() {
       <button>
         Logout
       </button>
+
+      <hr />
+
+      <h2>Your Hives</h2>
+
+      {hives.length === 0 ? (
+        <p>No hives yet.</p>
+      ) : (
+        hives.map((hive) => (
+          <div key={hive.id}>
+            <h3>{hive.name}</h3>
+
+            <p>Room Code: {hive.room_code}</p>
+
+            <button
+              onClick={() => navigate(`/hive/${hive.id}`)}
+            >
+              Open
+            </button>
+
+            <hr />
+          </div>
+        ))
+      )}
     </div>
   );
 }
